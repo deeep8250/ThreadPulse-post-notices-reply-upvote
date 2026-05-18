a1c9db945400328848104bab3a8ada71101508ca
deeep8250 <deepmondal825005@gmail.com>
2026-05-18 20:08:42 +0530

    added invalid inputs catch code

--- internal/middleware/error.go
+++ internal/middleware/error.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func ErrorHandler() gin.HandlerFunc {
@@ -12,6 +14,16 @@ func ErrorHandler() gin.HandlerFunc {
 
 		if len(c.Errors) > 0 {
 			err := c.Errors.Last()
+
+			var validationErr validator.ValidationErrors
+			if errors.As(err.Err, &validationErr) {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"status":  "error",
+					"message": "invalid input",
+				})
+				return
+			}
+
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"status":  "error",
 				"message": err.Error(),
